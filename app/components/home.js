fd4d59c72b06e4f9292a03e6d160cbce2993022c
febobo <febobo@zhangbodeMacBook-Pro.local>
2016-04-01 19:23:05 +0800

    布局

--- app/components/home.js
+++ app/components/home.js
@@ -3,7 +3,8 @@ import React, {
   Component,
   View,
   Text,
-  TouchableOpacity
+  TouchableOpacity,
+  TouchableHighlight
 } from 'react-native';
 
 export default class Home extends Component {
@@ -11,19 +12,102 @@ export default class Home extends Component {
    super(props);
  }
 
+ initNum (start , end){
+   let num = [1,2,3,4,5,6,7,8,9];
+   if(start) num = num.slice(start,end);
+   return {
+     num : num,
+     operator : ['/' , '*' , '-' ,'+' ,'=']
+   }
+ }
  render (){
    console.log(this.props)
    return (
-     <View>
-       <Text>111</Text>
-       <Text>111</Text>
-       <Text>111</Text>
-       <Text>111</Text>
-       <Text>111</Text>
-       <Text>111</Text>
-       <Text>111</Text>
-       <Text>111</Text>
+     <View style={[styles.containers , styles.leftTop]}>
+       <View style={[styles.containers]}>
+          <View style={{flex :3 , backgroundColor:'red'}}>
+              <View style={[styles.cell]}></View>
+              <View style={{flex : 3 , flexDirection:'column'}}>
+                <View style={{flex : 1, flexDirection : 'row'}}>
+                  {this.initNum(0,3).num.map( (v,k) =>
+                      <View style={[styles.operatorItem]}>
+                        <Text style={[styles.cell]}>{v}</Text>
+                      </View>
+                  )}
+                </View>
+                <View style={{flex : 1, flexDirection : 'row'}}>
+                  {this.initNum(3,6).num.map( (v,k) =>
+                      <View style={[styles.operatorItem]}>
+                        <Text style={[styles.cell]}>{v}</Text>
+                      </View>
+                  )}
+                </View>
+                <View style={{flex : 1, flexDirection : 'row'}}>
+                  {this.initNum(6,9).num.map( (v,k) =>
+                      <View style={[styles.operatorItem]}>
+                        <Text style={[styles.cell]}>{v}</Text>
+                      </View>
+                  )}
+                </View>
+              </View>
+              <View style={[styles.cell]}></View>
+          </View>
+          <View style={[styles.operator]}>
+            <View style={{flex: 1, flexDirection:'column', backgroundColor:"#eeeeee"}}>
+               {this.initNum().operator.map( (v,k) =>
+                   <View style={[styles.operatorItem]}>
+                      <Text style={[styles.operatorItemText]}>{v}</Text>
+                   </View>
+               )}
+            </View>
+          </View>
+       </View>
      </View>
    )
  }
 }
+// {this.initNum().operator.map( (v,k) =>
+//   <TouchableHighlight key={k}>
+//     <View sytle={{flex: 1, backgroundColor:"#aabbaa"}}>
+//       <Text >{v}</Text>
+//     </View>
+//   </TouchableHighlight>
+// )}
+
+let styles = StyleSheet.create({
+  leftTop : {
+    paddingTop : 80
+  },
+  cell :{
+    flex : 1
+  },
+  containers : {
+    flexDirection : 'row',
+    flex :1
+  },
+  rows :{
+    flexDirection : 'column',
+    flex : 1
+  },
+  num :{
+    flex : 3,
+    flexDirection : 'column',
+  },
+  operator : {
+    flex : 1,
+    flexDirection:'column',
+    backgroundColor : 'red'
+  },
+  operatorItem :{
+    flex : 1,
+    alignItems : 'center',
+    justifyContent : 'center',
+    borderWidth : 1,
+    backgroundColor : '#EF7B25'
+  },
+  operatorItemText : {
+    fontSize : 22,
+    color : '#fff'
+  }
+
+})
